73b800c0d27fe2e68564fe3faafc86826b35614e
Christoph Scholz <christoph.scholz@gmail.com>
2018-10-03 17:28:52 +0200

    now push messages arrive while device is idle

--- convFCMWakeup.py
+++ convFCMWakeup.py
@@ -14,5 +14,5 @@ push_service = FCMNotification(api_key = API_KEY)
 for row in c.execute('SELECT * FROM target'):
     device, domain, token, node, secret = row
     print(row)
-    result = push_service.notify_single_device(registration_id=token, data_message={"account": device}, collapse_key=device[:6])
+    result = push_service.notify_single_device(registration_id=token, data_message={"account": device}, collapse_key=device[:6], delay_while_idle=False)
     print(result)
