bcbd0a6c8191e03da10f60d1053b5a3b6e8cb871
Christoph Scholz <christoph.scholz@gmail.com>
2018-10-03 16:36:38 +0200

    first commit

---
+++ convFCMWakeup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from pyfcm import FCMNotification
+import sys
+import sqlite3
+
+# Your api-key can be gotten from:  https://console.firebase.google.com/project/<project-name>/settings/cloudmessaging
+API_KEY = "secret"
+
+c = sqlite3.connect('/var/lib/p2/TargetStore.db')
+push_service = FCMNotification(api_key = API_KEY)
+
+
+for row in c.execute('SELECT * FROM target'):
+    device, domain, token, node, secret = row
+    print(row)
+    result = push_service.notify_single_device(registration_id=token, data_message={"account": device}, collapse_key=device[:6])
+    print(result)
